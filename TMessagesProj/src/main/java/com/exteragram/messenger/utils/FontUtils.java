36791a28a894d14fdb06210b98fe0008df30f6b4
ZavaruKitsu <alexeyzavar@gmail.com>
2023-05-25 13:36:11 +0300

    fix: support fonts with weird bold

---
+++ TMessagesProj/src/main/java/com/exteragram/messenger/utils/FontUtils.java
@@ -0,0 +1,70 @@
+package com.exteragram.messenger.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+
+import org.telegram.messenger.AndroidUtilities;
+import org.telegram.messenger.FileLog;
+import org.telegram.messenger.LocaleController;
+
+import java.util.List;
+
+public class FontUtils {
+
+    private static final String TEST_TEXT;
+    private static final int CANVAS_SIZE = AndroidUtilities.dp(12);
+    private static final Paint PAINT = new Paint() {{
+        setTextSize(CANVAS_SIZE);
+        setAntiAlias(false);
+        setSubpixelText(false);
+        setFakeBoldText(false);
+    }};
+
+    private static Boolean mediumWeightSupported = null;
+    private static Boolean italicSupported = null;
+
+    static {
+        if (List.of("zh", "ja", "ko").contains(LocaleController.getInstance().getCurrentLocale().getLanguage())) {
+            TEST_TEXT = "日";
+        } else {
+            TEST_TEXT = "R";
+        }
+    }
+
+    public static boolean isMediumWeightSupported() {
+        if (mediumWeightSupported == null) {
+            mediumWeightSupported = testTypeface(Typeface.create("sans-serif-medium", Typeface.NORMAL));
+            FileLog.d("mediumWeightSupported = " + mediumWeightSupported);
+        }
+        return mediumWeightSupported;
+    }
+
+    public static boolean isItalicSupported() {
+        if (italicSupported == null) {
+            italicSupported = testTypeface(Typeface.create("sans-serif", Typeface.ITALIC));
+            FileLog.d("italicSupported = " + italicSupported);
+        }
+        return italicSupported;
+    }
+
+    private static boolean testTypeface(Typeface typeface) {
+        Canvas canvas = new Canvas();
+
+        Bitmap bitmap1 = Bitmap.createBitmap(CANVAS_SIZE, CANVAS_SIZE, Bitmap.Config.ALPHA_8);
+        canvas.setBitmap(bitmap1);
+        PAINT.setTypeface(null);
+        canvas.drawText(TEST_TEXT, 0, CANVAS_SIZE, PAINT);
+
+        Bitmap bitmap2 = Bitmap.createBitmap(CANVAS_SIZE, CANVAS_SIZE, Bitmap.Config.ALPHA_8);
+        canvas.setBitmap(bitmap2);
+        PAINT.setTypeface(typeface);
+        canvas.drawText(TEST_TEXT, 0, CANVAS_SIZE, PAINT);
+
+        boolean supported = !bitmap1.sameAs(bitmap2);
+        AndroidUtilities.recycleBitmaps(List.of(bitmap1, bitmap2));
+        return supported;
+    }
+}
+
